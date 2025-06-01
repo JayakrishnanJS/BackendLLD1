7c76bb9ee6fd3ee5774860746b7bc099f097dde9
Jayakrishnan J S <jayakrishnan.jkjs@gmail.com>
2025-06-02 00:01:05 +0530

    super and this keyword functionalities added for Inheritance

---
+++ src/main/java/Inheritance/Amphibians.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class Amphibians extends Animal{
+    // this() is used to call another constructor in the same class.
+    // It must be the first statement in the constructor.
+    // Useful for constructor chaining to reuse initialization logic.
+}
